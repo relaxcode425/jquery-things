42edebf3fbedd0dadccf1723305921c311eceaa0
Your Name <you@example.com>
2024-04-30 11:57:37 -0400

    aaaaaaaaaaaaa

--- js/funcion.js
+++ js/funcion.js
@@ -1,25 +1,6 @@
 $(document).ready(function () {
 
 
-    $("#btnEnviar").click(function () {
-        $("#formulario").submit();
-    })
-
-    $("#btnGenerar").click(function () {
-        let rut = $("#txtRut").val();
-        let nombre = $("#txtNombre").val();
-        let appaterno = $("#txtAppaterno").val();
-        let apmaterno = $("#txtApmaterno").val();
-        let fecha = $("#txtFecha").val();
-        let edad = $("#txtEdad").val();
-        let genero = $("input[name=opGenero]:checked").val();
-        let mail = $("#txtMail").val();
-        let telefono = $("#txtTelefono").val();
-        let comentario = $("#txtComentario").val();
-        console.log(genero)
-        validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, telefono)
-    })
-
     function validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, celular) {
 
         if (String(rut).length < 9 || String(rut).length > 10) {
@@ -39,16 +20,38 @@ $(document).ready(function () {
             $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Edad debe estar entre 18 y 30 años</div>");
         } else if (genero != "Masculino" && genero != "Femenino" && genero != "Otro") {
             /* Validacion Genero */
-            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Debe seleccionar un genero valido</div>");
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Debe seleccionar un género válido</div>");
 
         } else if (String(celular).length < 9 || String(celular).length > 12) {
             /* Validacion Edad */
-            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Telefono debe tener entre 9 y 12 digitos</div>");
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Teléfono debe tener entre 9 y 12 digitos</div>");
         } else {
             /* Confirmacion */
-            $("#check").html("<div class='alert alert-success w-50 mx-auto text-center' >Datos Ingresado con exito</div>");
+            $("#check").html("<div class='alert alert-success w-50 mx-auto text-center' >Datos ingresados con exito</div>");
 
         }
 
     }
+    
+    $("#btnEnviar").click(function () {
+        $("#formulario").submit();
+    })
+
+    $("#btnGenerar").click(function () {
+        let rut = $("#txtRut").val();
+        let nombre = $("#txtNombre").val();
+        let appaterno = $("#txtAppaterno").val();
+        let apmaterno = $("#txtApmaterno").val();
+        let fecha = $("#txtFecha").val();
+        let edad = $("#txtEdad").val();
+        let genero = $("input[name=opGenero]:checked").val();
+        let mail = $("#txtMail").val();
+        let telefono = $("#txtTelefono").val();
+        let profesion = $("#txtProf").val();
+        let comentario = $("#txtComentario").val();
+        console.log(genero)
+        validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, telefono)
+    })
+
+    
 });
