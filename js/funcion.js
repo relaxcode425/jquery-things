8e56065918a12bbe53007edb654090941e733392
Your Name <you@example.com>
2024-04-30 11:22:32 -0400

    h

---
+++ js/funcion.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+
+    $("#btnEnviar").click(function () {
+        $("#formulario").submit();
+    })
+
+    $("#btnGenerar").click(function () {
+        let rut = $("#txtRut").val();
+        let nombre = $("#txtNombre").val();
+        let appaterno = $("#txtAppaterno").val();
+        let apmaterno = $("#txtApmaterno").val();
+        let fecha = $("#txtFecha").val();
+        let edad = $("#txtEdad").val();
+        let genero = $("input[name=opGenero]:checked").val();
+        let mail = $("#txtMail").val();
+        let telefono = $("#txtTelefono").val();
+        let comentario = $("#txtComentario").val();
+        console.log(genero)
+        validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, telefono)
+    })
+
+    function validarDatos(rut, nombre, appaterno, apmaterno, edad, genero, celular) {
+
+        if (String(rut).length < 9 || String(rut).length > 10) {
+            /* Validacion Rut */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Rut debe tener largo entre 9 y 10 caracteres</div>");
+        } else if (String(nombre).length < 3 || String(nombre).length > 20) {
+            /* Validacion Nombre */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Nombre debe tener largo entre 3 y 20 caracteres</div>");
+        } else if (String(appaterno).length <= 3 || String(appaterno).length >= 20) {
+            /* Validacion Apellido Paterno */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Paterno debe tener largo entre 3 y 20 caracteres</div>");
+        } else if (String(apmaterno).length <= 3 || String(apmaterno).length >= 20) {
+            /* Validacion Apellido Materno */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Apellido Materno debe tener largo entre 3 y 20 caracteres</div>");
+        } else if (Number(edad) < 18 && Number(edad) > 30) {
+            /* Validacion Edad */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Edad debe estar entre 18 y 30 años</div>");
+        } else if (genero != "Masculino" && genero != "Femenino" && genero != "Otro") {
+            /* Validacion Genero */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Debe seleccionar un genero valido</div>");
+
+        } else if (String(celular).length < 9 || String(celular).length > 12) {
+            /* Validacion Edad */
+            $("#check").html("<div class='alert alert-danger w-50 mx-auto text-center' >Telefono debe tener entre 9 y 12 digitos</div>");
+        } else {
+            /* Confirmacion */
+            $("#check").html("<div class='alert alert-success w-50 mx-auto text-center' >Datos Ingresado con exito</div>");
+
+        }
+
+    }
+});
